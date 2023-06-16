7392cfa5675e9841e32b6c2ab851002e470de38c
altynaiabd <altynaiabdyrakhmanova@gmail.com>
2023-06-16 13:32:15 +0600

    add authentication

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 
-from apps.user.views import SignUpView
+from apps.user.views import SignUpView, LogInView, HomepageView
 
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
+    path('login/', LogInView.as_view(), name='login'),
+    path('', HomepageView.as_view(), name='homepage'),
+    path('', include('django.contrib.auth.urls')),
 ]
--- apps/user/views.py
+++ apps/user/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import CreateView
+from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
+from django.contrib.auth import authenticate, login, logout
+from django.views.generic import TemplateView
 
 from apps.user.forms import UserModelCreationForm
+
+
+class HomepageView(TemplateView):
+    template_name = 'pages/homepage.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_title'] = 'Главная'
+        return context
 
 
 class SignUpView(CreateView):
@@ -15,5 +27,26 @@ class SignUpView(CreateView):
         if form.is_valid():
             user = form.save(commit=False)
             user.save()
+
+            #выход из системы предыдущего пользователя
+            logout(request)
+
+            new_user = authenticate(username = form.cleaned_data['username'],
+                                    password = form.cleaned_data['password1'],
+                                    )
+            #Аутентификация и вход пользователя в систему после регистрации
+            login(request, new_user)
+
+            return redirect('homepage')
         else:
             return render(request, self.template_name, {'form': form})
+
+
+class LogInView(LoginView):
+    template_name = 'users/login.html'
+
+    # Перенаправление со страницы входа в систему в случае, если пользователь уже прошел аутентификацию и вошел в систему
+    def get(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('homepage')
+        return self.render_to_response(self.get_context_data())
